2a04a761bb4fd96a05937984379bd2c450ff8f47
gideon tobing <gideon.tobing@gmail.com>
2025-12-17 15:26:12 +1000

    add spacer

---
+++ ch02/spacer/spacer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spacer
+//
+//  Created by gideon tobing on 17/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           Spacer()
+            Text("one third of the way")
+            Spacer()
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ch02/spacer/spacer/spacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  spacerApp.swift
+//  spacer
+//
+//  Created by gideon tobing on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct spacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
